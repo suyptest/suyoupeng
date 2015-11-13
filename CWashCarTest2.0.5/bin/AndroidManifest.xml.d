5f0e0cdf2c14e334bf5d8e6a6136d75871119716
suyoupeng <suyptest@163.com>
2015-11-13 17:50:09 +0800

    更换文件夹

--- CWashCarTest2.0.5/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/suyoupeng/gitproject/suyoupeng/CWashCarTest2.0.5/bin/AndroidManifest.xml : \
-/Users/suyoupeng/gitproject/suyoupeng/CWashCarTest2.0.5/AndroidManifest.xml \
---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/suyoupeng/gitproject/suyoupeng/bin/AndroidManifest.xml : \
+/Users/suyoupeng/gitproject/suyoupeng/AndroidManifest.xml \
